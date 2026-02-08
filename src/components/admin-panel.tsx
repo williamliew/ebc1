87e2080883ea7dadf6ab3c3f3e254c9e9be4ff75
Will Liew <wliew@squiz.co.nz>
2026-02-09 08:05:33 +1100

    Added fill to svg

--- src/components/admin-panel.tsx
+++ src/components/admin-panel.tsx
@@ -8,7 +8,7 @@ function PenWritingIcon({ className }: { className?: string }) {
         <svg
             xmlns="http://www.w3.org/2000/svg"
             viewBox="0 0 24 24"
-            fill="none"
+            fill="white"
             stroke="currentColor"
             strokeWidth="2"
             strokeLinecap="round"
